50c83d2b4af0279d5b0c5f3a4f64c04b72d2649e
Joanna Filip <joanne.filip@gmail.com>
2021-03-18 11:04:53 +0100

    first commit

---
+++ Homeshare/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Homeshare
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/script.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/owl-carousel/js").Include(
+                     "~/owl-carousel/owl.carousel.js"));
+
+            bundles.Add(new ScriptBundle("~/slitslider/js").Include(
+                "~/slitslider/js/modernizr.custom.79639.js",
+                "~/slitslider/js/jquery.ba-cond.min.js",
+                "~/slitslider/js/jquery.slitslider.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/css/style.css"));
+
+            bundles.Add(new StyleBundle("~/owl-carousel").Include(
+                    "~/owl-carousel/owl.carousel.css",
+                    "~/owl-carousel/owl.theme.css"));
+
+            bundles.Add(new StyleBundle("~/slitslider/css").Include(
+                    "~/slitslider/css/style.css",
+                    "~/slitslider/css/custom.css"));
+            
+        }
+    }
+}
